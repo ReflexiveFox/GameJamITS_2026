45ff0d515e76715786725c6b6bc8e117a0ce43d2
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2026-05-12 15:25:43 +0200

    Outline + Selection logic

--- Assets/Scripts/SelectableEntity.cs
+++ Assets/Scripts/SelectableEntity.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 namespace GameJam
 {
+    [RequireComponent(typeof(Outline))]
     public abstract class SelectableEntity : MonoBehaviour, ISelectable
     {
         [SerializeField] private bool isSelected;
+        private Outline outline;
 
         public bool IsSelected => isSelected;
+
+        private void Awake()
+        {
+            outline = GetComponent<Outline>();
+        }
 
         public virtual void Select()
         {
             isSelected = true;
+            outline.enabled = true;
         }
 
         public virtual void Deselect()
         {
             isSelected = false;
+            outline.enabled = false;
         }
     }
 }
