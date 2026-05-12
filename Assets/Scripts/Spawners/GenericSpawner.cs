37f581370aad0999d4752699af79de7b4ec83422
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2026-05-12 21:14:50 +0200

    Exposing spawn offsets values in Generic Spawner

--- Assets/Scripts/Spawners/GenericSpawner.cs
+++ Assets/Scripts/Spawners/GenericSpawner.cs
@@ -4,9 +4,17 @@ namespace GameJam
 {
     public class GenericSpawner : MonoBehaviour
     {
+        [Header("Spawner Settings")]
         [SerializeField] private Entity prefab;
         [SerializeField] private float spawnInterval = 2f;
         [SerializeField] private int spawnCount = 1;
+        [Header("Spawn Area Settings")]
+        [Header("Horizontal Spawn Offset")]
+        [SerializeField] private float minXSpawnOffset = -.5f;
+        [SerializeField] private float maxXSpawnOffset = .5f;
+        [Header("Vertical Spawn Offset")]
+        [SerializeField] private float minZSpawnOffset = -.5f;
+        [SerializeField] private float maxZSpawnOffset = .5f;
 
         private float _timer;
 
@@ -24,7 +32,7 @@ namespace GameJam
         {
             for (int i = 0; i < spawnCount; i++)
             {
-                Vector3 randomOffset = new Vector3(Random.Range(-0.5f, 0.5f), 0f, Random.Range(-0.5f, 0.5f));
+                Vector3 randomOffset = new(Random.Range(minXSpawnOffset, maxXSpawnOffset), 0f, Random.Range(minZSpawnOffset, maxZSpawnOffset));
                 Instantiate(prefab, transform.position + randomOffset, transform.rotation);
             }
         }
